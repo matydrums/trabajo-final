f53da9107a18c713eaee977a009bfc346c3a37de
matydrums <matolmds@gmail.com>
2022-09-18 02:49:19 -0300

    mas archivos para el final

--- index.js
+++ index.js
@@ -1,2 +1,42 @@
 window.alert ('Bienvenidos a una pagina hecha con amor, para fanaticos de los palos y parches');
 
+
+
+
+
+function sound1(){
+ var snd = new
+Audio('sound/hithat.mp3')
+snd.play()
+}
+function sound2(){
+    var snd = new
+   Audio('sound/tacho.mp3')
+   snd.play()
+}
+function sound3(){
+    var snd = new
+   Audio('sound/plato.mp3')
+   snd.play()
+}
+function sound4(){
+    var snd = new
+   Audio('sound/tom2.mp3')
+   snd.play()
+}
+function sound5(){
+    var snd = new
+   Audio('sound/tom1.mp3')
+   snd.play()
+}
+function sound6(){
+    var snd = new
+   Audio('sound/bombo.mp3')
+   snd.play()
+}
+function sound7(){
+    var snd = new
+   Audio('sound/chancha.mp3')
+   snd.play()
+}
+
